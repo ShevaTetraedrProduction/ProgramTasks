613d606edd167ff3cb07b49de853c4e179b83ecf
Tetraedr <tetraedrgo@gmail.com>
2019-04-02 11:25:29 +0300

    change text condition in Problem40.java

--- src/Tasks/Problem40.java
+++ src/Tasks/Problem40.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Problem40 {
     public void start() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введіть степінь n \n 2^n");
+        System.out.println("Введіть степінь n (2^n)");
         double n = in.nextDouble();
         System.out.println(Math.pow(2,n));
 
