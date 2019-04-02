0b8d4fa984110c91dd06e2a0450ed3ff37612076
Tetraedr <tetraedrgo@gmail.com>
2019-04-02 11:22:13 +0300

    Add Problem40.java

--- src/Tasks/Problem40.java
+++ src/Tasks/Problem40.java
@@ -1,7 +1,13 @@
 package Tasks;
+import java.lang.Math;
+import java.util.Scanner;
 
 public class Problem40 {
     public void start() {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введіть степінь n \n 2^n");
+        double n = in.nextDouble();
+        System.out.println(Math.pow(2,n));
+
     }
 }
